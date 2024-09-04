e72d61505c60a33f7042fcad7d7454468fd3f49f
myudro33 <qanashvilinika7@gmail.com>
2024-09-04 18:32:18 +0400

    feat: fetch feedbacks from api

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,8 @@
 <script setup>
 import {  RouterView } from 'vue-router'
+import { usePostStore } from './stores/index';
+const postStore = usePostStore()
+postStore.getPosts()
 </script>
 
 <template>
