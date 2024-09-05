ac4a22702b26df69ce62c40f51b7794b99e55c68
myudro33 <qanashvilinika7@gmail.com>
2024-09-05 18:41:42 +0400

    feat: add function for feedback likes

--- src/stores/actions.js
+++ src/stores/actions.js
@@ -1,4 +1,19 @@
 export default {
+  async addLike(id) {
+    const index = (element) => element.id == id
+    const item = this.posts.findIndex(index)
+    await fetch(`http://localhost:3000/posts/${id}`, {
+      method: 'PUT',
+      body: JSON.stringify({
+        ...this.posts[item],
+        likes: this.posts[item].liked
+          ? (this.posts[item].likes -= 1)
+          : (this.posts[item].likes += 1),
+        liked: this.posts[item].liked ? false : true
+      })
+    })
+    this.posts[item].liked = !this.posts[item].liked
+  },
   async addPost(data) {
     const feedback = {
       id: String(Math.floor(Math.random() * 99999)),
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,37 +1,44 @@
 <script setup>
 import SidebarComponent from '../components/SidebarComponent.vue'
 import NavbarComponent from '../components/NavbarComponent.vue'
-import { usePostStore } from '@/stores';
-import ArrowUp from '@/components/icons/ArrowUp.vue';
-import CommentIcon from '@/components/icons/CommentIcon.vue';
- const postStore = usePostStore()
+import { usePostStore } from '@/stores'
+import ArrowUp from '@/components/icons/ArrowUp.vue'
+import CommentIcon from '@/components/icons/CommentIcon.vue'
+const postStore = usePostStore()
 </script>
 
 <template>
- <div class="w-screen h-screen bg-red-500 pt-24 px-52 flex">
-  <SidebarComponent/>
-    <div class="w-[60vw] ml-10  h-full flex flex-col">
-      <NavbarComponent/>
-      <div v-for="(item,index) in postStore.posts" :key="index" class="w-full h-40 flex justify-between rounded-xl p-8 bg-green-700 mt-5">
-       <div class="flex">
-         <div class="w-10 h-16 p-3 rounded-lg bg-red-500 flex flex-col items-center"> 
-           <ArrowUp/>
-           <h1 class="mt-2">{{ item.likes }}</h1>
+  <div class="w-screen h-screen bg-red-500 pt-24 px-52 flex">
+    <SidebarComponent />
+    <div class="w-[60vw] ml-10 h-full flex flex-col">
+      <NavbarComponent />
+      <div
+        v-for="(item, index) in postStore.posts"
+        :key="index"
+        class="w-full h-40 flex justify-between rounded-xl p-8 bg-green-700 mt-5"
+      >
+        <div class="flex">
+          <div
+            @click="postStore.addLike(item.id)"
+            v-bind:class="[item.liked ? 'bg-blue-500' : 'bg-red-500']"
+            class="w-10 h-16 p-3 rounded-lg flex flex-col items-center"
+          >
+            <ArrowUp />
+            <h1 class="mt-2">{{ item.likes }}</h1>
           </div>
           <div class="ml-10">
             <h1>{{ item.title }}</h1>
-            <p>{{item.description}}</p>
-            <span>{{item.type}}</span>
+            <p>{{ item.description }}</p>
+            <span>{{ item.type }}</span>
           </div>
         </div>
         <div class="flex items-center">
-          <CommentIcon/>
+          <CommentIcon />
           <h1 class="ml-2 font-bold">
             {{ item.comments.length }}
           </h1>
         </div>
-
       </div>
+    </div>
   </div>
- </div>
 </template>
