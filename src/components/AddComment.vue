1b8600a0b0c8e26b62b98bfae71aee9ad949b1d3
myudro33 <qanashvilinika7@gmail.com>
2024-09-28 23:59:56 +0400

    feat: set coment input to empty on comment add

--- src/components/AddComment.vue
+++ src/components/AddComment.vue
@@ -3,7 +3,7 @@
     <h1>Add comment</h1>
     <textarea v-model="text" class="resize-none" name="text"></textarea>
     <div class="flex w-full justify-between">
-      <p>255characters left</p>
+      <p>{{ 255 - text.length }} characters left</p>
       <button @click="AddComment(props.id)" class="bg-blue-200">post comment</button>
     </div>
   </div>
@@ -17,5 +17,6 @@ const props = defineProps(['id', 'inner', 'postId'])
 const text = ref('')
 const AddComment = (id) => {
   store.AddComment(id, text.value)
+  text.value = ''
 }
 </script>
