bb944cfd96dfb11673b2ed60cb7bc5116cf3c993
myudro33 <qanashvilinika7@gmail.com>
2024-10-18 14:55:15 +0400

    feat: add new route for roadmap page

--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,7 @@ import HomeView from '../views/HomeView.vue'
 import AddFeedbackView from '@/views/AddFeedbackView.vue'
 import FeedbackDetail from '@/views/FeedbackDetail.vue'
 import EditFeedbackView from '@/views/EditFeedbackView.vue'
+import RoadmapView from '@/views/RoadmapView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -26,6 +27,11 @@ const router = createRouter({
       path: '/feedback/edit/:id',
       name: 'edit-feedback',
       component: EditFeedbackView
+    },
+    {
+      path: '/roadmap',
+      name: 'roadmap',
+      component: RoadmapView
     }
   ]
 })
---
+++ src/views/RoadmapView.vue
@@ -0,0 +1,68 @@
+<template>
+  <div class="px-44 py-28">
+    <div class="w-full h-40 bg-red-500 rounded-2xl p-10 flex items-center justify-between">
+      <div>
+        <router-link to="/">go back</router-link>
+        <h1 class="text-3xl font-bold mt-2">Roadmap</h1>
+      </div>
+      <RouterLink
+        :to="{ name: 'add-feedback' }"
+        class="p-4 font-bold h-14 bg-purple-600 flex items-center rounded-xl"
+        >+ Add Feedback</RouterLink
+      >
+    </div>
+    <div class="w-full flex justify-between mt-20">
+      <div class="w-[32%]">
+        <RoadmapCard
+          v-for="(item, index) in postStore.getPlannedPosts"
+          :key="index"
+          :id="item.id"
+          :description="item.description"
+          :liked="item.liked"
+          :likes="item.likes"
+          :title="item.title"
+          :type="item.type"
+          :status="item.status"
+          color="orange"
+          :comments="item.comments.length"
+        />
+      </div>
+      <div class="w-[32%]">
+        <RoadmapCard
+          v-for="(item, index) in postStore.getProgressPosts"
+          :key="index"
+          :id="item.id"
+          :description="item.description"
+          :liked="item.liked"
+          :likes="item.likes"
+          :title="item.title"
+          :type="item.type"
+          :status="item.status"
+          color="purple"
+          :comments="item.comments.length"
+        />
+      </div>
+      <div class="w-[32%]">
+        <RoadmapCard
+          v-for="(item, index) in postStore.getLivePosts"
+          :key="index"
+          :id="item.id"
+          :description="item.description"
+          :liked="item.liked"
+          :likes="item.likes"
+          :title="item.title"
+          :type="item.type"
+          :status="item.status"
+          color="blue"
+          :comments="item.comments.length"
+        />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import RoadmapCard from '@/components/RoadmapCard.vue'
+import { usePostStore } from '@/stores'
+const postStore = usePostStore()
+</script>
